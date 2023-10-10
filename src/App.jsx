f72ca0284eb78fc19a0d7e54fbf2780a51b72481
gabriel ward <mokeward47@gmail.com>
2023-10-10 15:44:26 -0600

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
 export default function App() {
-  return
-   <div>Hello</div>;
+  
+
+  const [friends, setFriends] = useState([])
+  const [picture, setPicture] = useState('')
+  const [name, setName] = useState('')
+
+
+  async function getSavedFriends( ) {
+    axios.get('/api/friends')
+    .then((res) => {
+      setFriends(res.data)
+    })
+  }
+
+     useEffect(() => getSavedFriends, [])
+
+  const addFriends =( ) => {
+    const newFriend = [...friends] 
+    newFriend.push ({ picture: picture, name: name})
+    setFriends (newFriend)
+    setName('')
+    setPicture('')
+  }
+const friendInfo = friends.map((friend) => {
+  return(
+  <div key = {friend.name}>
+  <img src={friend.picture} alt= {friend.name} />
+  <span>{friend.name}</span>
+  </div>)
+})
+  return<div>
+    <label htmlFor ="imgUrl">picture</label>
+    <input type= "text" id = "picture" value = {picture} onChange={(e)  => setPicture(e.target.value)}/>
+    <label htmlFor="Nameinput">Name</label>
+    <input id="NameInput" type="text" value = {name} onChange={(e) => setName(e.target.value)}/>
+    <button id = "Add Friend" onClick={addFriends}>Add friend here</button>
+    {friendInfo}
+
+  </div>;
   
 }
